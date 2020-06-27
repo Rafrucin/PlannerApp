400bd94e105d4a7f6198721fab229ce440cdf44d
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-06-27 22:30:13 +0100

    ok

--- PlannerApp.Shared/Models/ToDoItem.cs
+++ PlannerApp.Shared/Models/ToDoItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PlannerApp.Shared.Models
@@ -7,6 +8,8 @@ namespace PlannerApp.Shared.Models
     public class ToDoItem
     {
         public string Id { get; set; }
+
+        [MaxLength(200)]
         public string Description { get; set; }
         public bool IsDone { get; set; }
         public DateTime? EstimatedDate { get; set; }
